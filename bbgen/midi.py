803efa6965181e9e7bd790aa71f88f12c035ab96
Hay Kranen <hay@haykranen.nl>
2023-11-30 14:17:02 +0100

    Rewriting midi to use mido, adding set_program method

--- bbgen/midi.py
+++ bbgen/midi.py
@@ -1,6 +1,7 @@
 from bbgen.soundfont import Soundfont
 from bbgen.wave import Wave
 from midi2audio import FluidSynth
+from mido import MidiFile, MidiTrack, Message
 from music21 import converter
 from pathlib import Path
 from tempfile import NamedTemporaryFile
@@ -10,6 +11,7 @@ TINY_NOTATION_PREFIX = "tinyNotation:"
 class Midi:
     def __init__(self, path: Path):
         self.path = path
+        self.midi = MidiFile(self.path)
 
     # Render Tinynotation as a new MIDI file
     @classmethod
@@ -26,7 +28,27 @@ class Midi:
             return cls(file.name)
 
     def render(self, soundfont: Soundfont) -> Wave:
-        with NamedTemporaryFile() as file:
-            FluidSynth(soundfont.path).midi_to_audio(self.path, file.name)
-            file.seek(0)
-            return Wave.from_data(file.read())
+        # Create temp files for in/out files
+        in_file = NamedTemporaryFile()
+        out_file = NamedTemporaryFile()
+
+        # First write midi data to file
+        self.midi.save(in_file.name)
+
+        # Now render
+        FluidSynth(soundfont.path).midi_to_audio(in_file.name, out_file.name)
+
+        # Fetch data and return that as a new Wave object
+        out_file.seek(0)
+        wave_data = out_file.read()
+
+        # Make sure to close the file handles
+        in_file.close()
+        out_file.close()
+
+        return Wave.from_data(wave_data)
+
+    def set_program(self, track_index: int, program: int):
+        message = Message('program_change', program = program)
+        self.midi.tracks[track_index].insert(0, message)
+        return self
--- test/example.py
+++ test/example.py
@@ -5,6 +5,6 @@ from bbgen.soundfont import Soundfont
 from bbgen.midi import Midi
 from bbgen.paulstretch import Paulstretch
 
-arp1 = Midi("arp1.mid")
+arp1 = Midi("arp1.mid").set_program(0, 90)
 audio = arp1.render(soundfont = Soundfont("./FluidR3Mono_GM.sf3"))
 audio.effect(Paulstretch()).write("arp1.mp3", format = "mp3")

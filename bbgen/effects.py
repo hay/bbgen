b5a979d3323e5c3bb2af03159293982d2f669c48
Hay Kranen <hay@haykranen.nl>
2023-12-02 21:18:46 +0100

    Adding working pedalboardprocessor

--- bbgen/effects.py
+++ bbgen/effects.py
@@ -1,4 +1,5 @@
 from bbgen.paulstretch import Paulstretch
 from bbgen.pitchshift import PitchShift
 from bbgen.playbackspeed import Playbackspeed
+from bbgen.pprocessor import PedalboardProcessor
 from bbgen.timestretch import TimeStretch
---
+++ bbgen/pprocessor.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from pedalboard import Pedalboard
+from pedalboard.io import AudioFile
+from pydub import AudioSegment
+from tempfile import NamedTemporaryFile
+
+@dataclass
+class PedalboardProcessor:
+    board: Pedalboard
+
+    def apply(self, segment:AudioSegment) -> AudioSegment:
+        print(f"Applying Pedalboard")
+
+        # FIXME: this must somehow get more efficient
+        in_file = NamedTemporaryFile(suffix = ".wav")
+        out_file = NamedTemporaryFile(suffix = ".wav")
+
+        segment.export(in_file.name, format = "wav")
+
+        with AudioFile(in_file.name) as inp:
+            with AudioFile(out_file.name, 'w', inp.samplerate, inp.num_channels) as out:
+                while inp.tell() < inp.frames:
+                    chunk = inp.read(inp.samplerate)
+                    effected = self.board(chunk, inp.samplerate, reset = False)
+                    out.write(effected)
+
+        new_segment = AudioSegment.from_wav(out_file.name)
+
+        in_file.close()
+        out_file.close()
+
+        return new_segment

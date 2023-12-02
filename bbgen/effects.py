80bda919f8e0d0c80f182729a110a553ce457fa7
Hay Kranen <hay@haykranen.nl>
2023-12-02 22:38:58 +0100

    Fixing effects

--- bbgen/effects.py
+++ bbgen/effects.py
@@ -1,5 +1,4 @@
 from bbgen.paulstretch import Paulstretch
-from bbgen.pitchshift import PitchShift
 from bbgen.playbackspeed import Playbackspeed
 from bbgen.pprocessor import PedalboardProcessor
 from bbgen.timestretch import TimeStretch
--- test/pboard.py
+++ test/pboard.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.append("..")
 
-from bbgen.effects import PedalboardProcessor, Playbackspeed
+from bbgen.effects import PedalboardProcessor, Playbackspeed, TimeStretch
 from pedalboard import Pedalboard, Chorus, Reverb, Phaser
 from pydub import AudioSegment
 
 board = Pedalboard([Phaser(), Chorus(), Reverb(room_size=1)])
-chico = AudioSegment.from_mp3("chico.mp3")[0:1000] + AudioSegment.silent(duration = 3000)
+chico = AudioSegment.from_mp3("chico.mp3")[1000:2000] + AudioSegment.silent(duration = 3000)
 chico = PedalboardProcessor(board).apply(chico)
-chico = Playbackspeed(0.5).apply(chico)
+chico = Playbackspeed(.25).apply(chico)
+chico = TimeStretch(pitch = 12).apply(chico)
 chico.export("chico-pboard.mp3")
